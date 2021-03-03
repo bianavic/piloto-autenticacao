63bd2e0ed0b314d3e36f446c57b880ba11370046
fabferlin <fabferlin@gmail.com>
2021-03-03 18:22:28 -0300

    refatoracao

---
+++ src/main/java/com/piloto/autenticacao/controller/RegisterController.java
@@ -0,0 +1,25 @@
+package com.piloto.autenticacao.controller;
+
+import com.piloto.autenticacao.model.User;
+import com.piloto.autenticacao.services.RegisterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+
+@RestController
+public class RegisterController {
+
+    @Autowired
+    private RegisterService service;
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST, produces = {"application/json"})
+    public String registration(@Valid @RequestBody User user){
+
+        service.insert(user);
+
+        return "User registered successfully";
+    }
+    
+}

b0bc12d9de99276c995d83e2403a4c83a7f396e8
bianavic <bianavic@gmail.com>
2021-03-13 15:45:48 -0300

    implementing swagger configuration

--- src/main/java/com/piloto/autenticacao/config/SwaggerConfig.java
+++ src/main/java/com/piloto/autenticacao/config/SwaggerConfig.java
@@ -1,19 +1,10 @@
 package com.piloto.autenticacao.config;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
-import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
